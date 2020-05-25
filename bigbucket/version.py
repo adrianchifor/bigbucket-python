7f2b53d7eb053c8689cf70d00918dd2fff34c5e2
Adrian Chifor <me@adrianchifor.com>
2020-05-26 00:04:17 +0100

    Version increment 0.3.0

--- bigbucket/version.py
+++ bigbucket/version.py
@@ -1,7 +1,7 @@
 title = 'bigbucket'
 description = 'Python3 client for Bigbucket'
 url = 'https://github.com/adrianchifor/bigbucket-python'
-version = '0.2.0'
+version = '0.3.0'
 author = 'Adrian Chifor'
 author_email = 'me@adrianchifor.com'
 license = 'GNU General Public License v3 (GPLv3)'
