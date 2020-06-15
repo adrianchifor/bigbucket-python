b3eb09f915a8091695626bb19976fc6479eb34fb
Adrian Chifor <me@adrianchifor.com>
2020-06-15 01:04:34 +0100

    Use requests-gcp lib

--- bigbucket/client.py
+++ bigbucket/client.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-import requests
+import requestsgcp as requests
 
-from bigbucket.auth import authenticate
 from bigbucket.utils import parse_response, handle_exception
 
 
@@ -13,13 +12,12 @@ class Client(object):
         self.timeout = timeout
         self.gcp_auth = gcp_auth
         self.headers = request_headers
-        self.jwt = None
 
-    @authenticate
     def get_tables(self) -> list:
         try:
             r = requests.get(f"{self.address}/api/table",
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
@@ -27,7 +25,7 @@ class Client(object):
 
     def table(self, table_name: str) -> Table:
         return Table(table_name, address=self.address, timeout=self.timeout,
-                     gcp_auth=self.gcp_auth, headers=self.headers, jwt=self.jwt)
+                     gcp_auth=self.gcp_auth, headers=self.headers)
 
 
 class Table(object):
@@ -37,81 +35,79 @@ class Table(object):
         self.timeout = kwargs.get("timeout")
         self.gcp_auth = kwargs.get("gcp_auth")
         self.headers = kwargs.get("headers")
-        self.jwt = kwargs.get("jwt")
 
-    @authenticate
     def delete_table(self):
         try:
             params = {"table": self.table_name}
             r = requests.delete(f"{self.address}/api/table", params=params,
-                                timeout=self.timeout, headers=self.headers)
+                                timeout=self.timeout, headers=self.headers,
+                                gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         parse_response(r)
 
-    @authenticate
     def list_columns(self) -> list:
         try:
             params = {"table": self.table_name}
             r = requests.get(f"{self.address}/api/column", params=params,
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         return parse_response(r)["columns"]
 
-    @authenticate
     def delete_column(self, column_name: str):
         try:
             params = {"table": self.table_name, "column": column_name}
             r = requests.delete(f"{self.address}/api/column", params=params,
-                                timeout=self.timeout, headers=self.headers)
+                                timeout=self.timeout, headers=self.headers,
+                                gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         parse_response(r)
 
-    @authenticate
     def count_rows(self, prefix: str = None) -> int:
         try:
             params = {"table": self.table_name}
             if prefix:
                 params["prefix"] = prefix
             r = requests.get(f"{self.address}/api/row/count", params=params,
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         return int(parse_response(r)["rowsCount"])
 
-    @authenticate
     def list_rows(self, prefix: str = None) -> list:
         try:
             params = {"table": self.table_name}
             if prefix:
                 params["prefix"] = prefix
             r = requests.get(f"{self.address}/api/row/list", params=params,
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         return parse_response(r)["rowKeys"]
 
-    @authenticate
     def read_row(self, key: str, columns: list = None) -> dict:
         try:
             params = {"table": self.table_name, "key": key}
             if columns:
                 params["columns"] = ",".join(columns)
             r = requests.get(f"{self.address}/api/row", params=params,
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         return parse_response(r)[key]
 
-    @authenticate
     def read_rows(self, prefix: str = None, columns: list = None, limit: int = None) -> dict:
         try:
             params = {"table": self.table_name}
@@ -122,40 +118,41 @@ class Table(object):
             if limit:
                 params["limit"] = limit
             r = requests.get(f"{self.address}/api/row", params=params,
-                             timeout=self.timeout, headers=self.headers)
+                             timeout=self.timeout, headers=self.headers,
+                             gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         return parse_response(r)
 
-    @authenticate
     def set_row(self, key: str, column_value_map: dict):
         try:
             params = {"table": self.table_name, "key": key}
             r = requests.post(f"{self.address}/api/row", params=params, json=column_value_map,
-                              timeout=self.timeout, headers=self.headers)
+                              timeout=self.timeout, headers=self.headers,
+                              gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         parse_response(r)
 
-    @authenticate
     def delete_row(self, key: str):
         try:
             params = {"table": self.table_name, "key": key}
             r = requests.delete(f"{self.address}/api/row", params=params,
-                                timeout=self.timeout, headers=self.headers)
+                                timeout=self.timeout, headers=self.headers,
+                                gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
         parse_response(r)
 
-    @authenticate
     def delete_rows(self, prefix: str):
         try:
             params = {"table": self.table_name, "prefix": prefix}
             r = requests.delete(f"{self.address}/api/row", params=params,
-                                timeout=self.timeout, headers=self.headers)
+                                timeout=self.timeout, headers=self.headers,
+                                gcp_auth=self.gcp_auth)
         except Exception as e:
             handle_exception(e)
 
